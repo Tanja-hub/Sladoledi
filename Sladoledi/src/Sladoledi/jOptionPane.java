7ad21268f463a921e39af5b4814b13527fe2ccf9
Tanja-hub <69401800+Tanja-hub@users.noreply.github.com>
2020-08-09 13:29:26 +0200

    Add files via upload

---
+++ Sladoledi/src/Sladoledi/jOptionPane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sladoledi;
+
+/**
+ *
+ * @author Tanja
+ */
+class jOptionPane {
+
+    static void showMessageDialog(icecream aThis, String ocekujte_dostavu_u_roku_od_30_min) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Sladoledi/src/Sladoledi/jToggleButton1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sladoledi;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Tanja
+ */
+class jToggleButton1 {
+
+    static void setText(String žuta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setBackground(Color GRAY) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
